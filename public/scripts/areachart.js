83c5009641c5f912004c4844899a589a2c83f7f0
MikRahVin <m.vintherr@gmail.com>
2024-05-29 13:05:37 +0200

    Changed area chart to display percentages instead

--- public/scripts/areachart.js
+++ public/scripts/areachart.js
@@ -21,15 +21,16 @@
 
         const svg = d3.select("#areaChart")
             .attr("width", areaW)
-            .attr("height", areaH + margin.bottom)  
+            .attr("height", areaH + margin.bottom)
             .append("g")
-            .attr("transform", `translate(${margin.top})`);
+            .attr("transform", `translate(${margin.left},${margin.top})`);
 
         const x = d3.scaleLinear()
             .range([margin.left, areaW - margin.right]);
 
         const y = d3.scaleLinear()
-            .range([areaH - margin.bottom, margin.top]);
+            .range([areaH - margin.bottom, margin.top])
+            .domain([0, 100]);
 
         const color = d3.scaleOrdinal()
             .domain(["Fossil_fuel", "Nuclear_electricity", "Renewable_electricity"])
@@ -37,13 +38,13 @@
 
         const area = d3.area()
             .x(d => x(d.data.Year))
-            .y0(d => y(d[0]))
-            .y1(d => y(d[1]));
+            .y0(d => y(d[0] * 100))
+            .y1(d => y(d[1] * 100));
 
         const stack = d3.stack()
             .keys(["Fossil_fuel", "Nuclear_electricity", "Renewable_electricity"])
             .order(d3.stackOrderNone)
-            .offset(d3.stackOffsetNone);
+            .offset(d3.stackOffsetExpand);
 
         // List of specific countries to display
         const specificContinents = ["Africa", "Asia", "South America", "North America", "Oceania", "Europe", "World"];
@@ -68,50 +69,48 @@
 
         function updateChart() {
             const continentData = filteredData.filter(d => d.Entity === selectedContinent);
-        
+
             x.domain(d3.extent(continentData, d => d.Year));
-            y.domain([0, d3.max(continentData, d => d.Fossil_fuel + d.Nuclear_electricity + d.Renewable_electricity)]).nice();
-        
+
             const series = stack(continentData);
-        
+
             svg.selectAll("path").remove();
             svg.selectAll(".x-axis").remove();
             svg.selectAll(".y-axis").remove();
-        
+
             const areas = svg.append("g")
                 .selectAll("path")
                 .data(series)
                 .enter().append("path")
                 .attr("fill", ({ key }) => color(key));
-        
+
             // Initialize the paths at the bottom of the chart
             areas.attr("d", area.y0(d => y(0)).y1(d => y(0)))
                 // Transition to the actual values
                 .transition()
                 .ease(d3.easeExpOut)
                 .duration(1000)  // Duration of the animation in milliseconds
-                .attr("d", area.y0(d => y(d[0])).y1(d => y(d[1])))
+                .attr("d", area)
                 .end()
                 .then(() => {
                     // After the animation completes, add titles
                     areas.append("title")
                         .text(({ key }) => key);
                 });
-        
+
             // Add the x-axis
             svg.append("g")
                 .attr("transform", `translate(0,${areaH - margin.bottom})`)
                 .attr("class", "x-axis")
                 .call(d3.axisBottom(x).ticks(areaW / 80).tickFormat(d3.format("d")).tickSizeOuter(0));
-        
+
             // Add the y-axis
             svg.append("g")
                 .attr("transform", `translate(${margin.left},0)`)
                 .attr("class", "y-axis")
-                .call(d3.axisLeft(y));
+                .call(d3.axisLeft(y).ticks(10).tickFormat(d => d + '%'));
         }
-        
-        
+
         africaBtn.addEventListener("click", () => {
             selectedContinent = "Africa";
             updateChart();
@@ -140,9 +139,8 @@
             selectedContinent = "World";
             updateChart();
         });
-        
+
         // Initialize the chart with the first country
         updateChart();
     });
-
 })();
