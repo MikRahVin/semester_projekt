84e235e9bebbe699e9e7b7beb9dc3553fdc769d8
MikRahVin <m.vintherr@gmail.com>
2024-05-29 16:04:32 +0200

    Updated chart with data base

--- public/scripts/bubble.js
+++ public/scripts/bubble.js
@@ -60,59 +60,25 @@ fetch(apiUrl)
     .then(data => {
         console.log(data)
 
-        
-// definign continentsObj. It's defining an object with a property called children. Children holds an array of objects of each continent.
-// Each continent also has a property called children, which holds objects of each country that lies within that continent.
-// The continents objects holds the average values for each electricity types for their respective countries.
-        let continentsObj = {
-            "name": "continents",
-            "children": [
-                { "name": "Asia", "value": 0, "fossil": 0, "nuclear": 0, "renewable": 0, "children": [] },
-                { "name": "Europe", "value": 0, "fossil": 0, "nuclear": 0, "renewable": 0, "children": [] },
-                { "name": "North America", "value": 0, "fossil": 0, "nuclear": 0, "renewable": 0, "children": [] },
-                { "name": "South America", "value": 0, "fossil": 0, "nuclear": 0, "renewable": 0, "children": [] },
-                { "name": "Oceania", "value": 0, "fossil": 0, "nuclear": 0, "renewable": 0, "children": [] },
-                { "name": "Africa", "value": 0, "fossil": 0, "nuclear": 0, "renewable": 0, "children": [] }
-            ]
+        console.log("this is new continent data", data)
+
+
+
+        let i = 0;
+        let continentObj = [];
+        while (i < data.length) {
+            if (data[i].year === 2021) {
+                let continent = new ContinentInsert(
+                    data[i].continent,
+                    data[i].fossil_fuel + data[i].nuclear_electricity + data[i].renewable_electricity
+                );
+                continentObj.push(continent);
+            }
+            i++;
         }
-
-// using forEach() to loop over every country in our data and then instantiating our constructor function CountryInsert(it's defined in the bottom of the document.)
-//Each instantation is declared as a variable called countryObj. We're then allocating the countries to each of their respective continents.
-// the logic used: we're using .find to check if the continent.name in continentsObj matches the continent name for the country. If true, then the path to that given continent object
-// gets declared as a variable called continentFound. We're then pushing our countryObj to continentfound.children.
-        data.forEach(country => {
-            let countryObj = new CountryInsert(country.continent,
-                country.country,
-                country.fossil_fuel + country.nuclear_electricity + country.renewable_electricity,
-                country.renewable_electricity,
-                country.nuclear_electricity,
-                country.fossil_fuel);
-            let continentFound = continentsObj.children.find(continent => continent.name === country.continent);
-            if (continentFound) {
-                continentFound.children.push(countryObj);
-            }
-        });
-
-//loggin continentsObj to make sure that it's working
-        console.log(continentsObj)
-
-//Here we're calculating average electricity generated per capita for each continent. We're using a forEach loop over continentsObj to automate the process. 
-// We're delcating 4 variables, one for each electricity type and the total of them all. If we look at renewable, then we're using .reduce on continent.children. 
-//it adds up all the renewable electricity values for each country into our accumalator. Then we're defining continent.renewable to renewable divided with 
-//continent.children.length to find the average. This process is repeated for each electricity type and each continent.
-        continentsObj.children.forEach(continent => {
-            if (continent.children.length > 0) {
-                const total = continent.children.reduce((acc, country) => acc + country.value, 0);
-                continent.value = total / continent.children.length;
-                const renewable = continent.children.reduce((acc, country) => acc + country.renewable, 0);
-                continent.renewable = renewable / continent.children.length;
-                const nuclear = continent.children.reduce((acc, country) => acc + country.nuclear, 0);
-                continent.nuclear = nuclear / continent.children.length;
-                const fossil = continent.children.reduce((acc, country) => acc + country.fossil, 0);
-                continent.fossil = fossil / continent.children.length;
-            }
-        });
-
+        
+        console.log(continentObj);
+        
 
 
     
@@ -127,7 +93,7 @@ fetch(apiUrl)
 //creating a scale for our bubble chart where the domain goes from 0 up to the maximum avereage total electricity generated per capita.
 // then we're delcaring the range to be from 10 to 200 to make it easier to work with.
         const bubbleScale = d3.scaleSqrt()
-            .domain([0, d3.max(continentsObj.children, d => d.value)])
+            .domain([0, d3.max(continentObj, d => d.value)])
             .range([10, 175]);
 
 
@@ -137,7 +103,7 @@ fetch(apiUrl)
 //.force collision This is where the magic heppens. We're defining the radius of each continent bubbles. Then we're defining the strength of the repulsion and then how
 //many times we're iterating to make the animation smooth.
 //.on tick is defined later in the script. But essentially it makes sure that, we keep updating the animation.
-        let simulation = d3.forceSimulation(continentsObj.children)
+        let simulation = d3.forceSimulation(continentObj)
             .force("charge", d3.forceManyBody().strength(200)) 
             .force("center", d3.forceCenter(bubbleW / 2, bubbleH / 2))
             .force("collision", d3.forceCollide().radius(d => bubbleScale(d.value) + 10).strength(1.2).iterations(32))
@@ -145,12 +111,12 @@ fetch(apiUrl)
 
 // appending each bubble
         const bubbles = svg3.selectAll("circle")
-            .data(continentsObj.children)
+            .data(continentObj)
             .enter()
             .append("circle")
             //Setting id for africa's bubble as africaCircle
             .attr("id", d => {
-                if (d.name === "Africa") {
+                if (d.continent === "Africa") {
                     return "africaCircle"
                 }
             })
@@ -159,7 +125,7 @@ fetch(apiUrl)
             .attr("cx", bubbleW / 2)  // Temporarily set to center
             .attr("cy", bubbleH / 2)  // Temporarily set to center
             //if data.name = to africa then set the color to steelblue, otherwise it should be #c4c4c4
-            .attr("fill", d => d.name === "Africa" ? "steelblue" : "hsl(207, 44%, 75%)")
+            .attr("fill", d => d.continent === "Africa" ? "steelblue" : "hsl(207, 44%, 75%)")
             // .call is a method that invokes functions on the selected elements. these functions are defined elsewhere in the script.
             .call(d3.drag()
                 .on("start", dragstarted)
@@ -169,16 +135,17 @@ fetch(apiUrl)
 
     // append labels to each bubble and setting their x and why values to the center of the bubbles. 
         const labels = svg3.selectAll("text")
-            .data(continentsObj.children)
+            .data(continentObj)
             .enter().append("text")
             .attr("x", d => d.x)
             .attr("y", d => d.y)
             .attr("text-anchor", "middle")
+            .attr("font-size", "12px")
             // bolding the text of africas bubble
-            .style("font-weight", d => d.name === "Africa" ? "bold" : "")
+            .style("font-weight", d => d.continent === "Africa" ? "bold" : "")
             .style("fill", "#fff")
             //setting the label to be the name and the value of each continent.
-            .text(d => `${d.name}: ${Math.round(d.value)} kwh`);
+            .text(d => `${d.continent}: ${Math.round(d.value)} kwh`);
 
 
 
@@ -236,15 +203,15 @@ function dragended(event, d) {
 
 //declaring the function setEnergy. It's what we use to increase and decrease the size of the africa bubble according to how much value is added to the africa bubble.
         function setEnergy(amount) {
-            let africa = continentsObj.children.find(continent => continent.name === "Africa");
+            let africa = continentObj.find(d => d.continent === "Africa");
             if (africa) {
                 africa.value = amount; // Set the value for Africa
             }
             // Temporarily stop the simulation to avoid conflicts during transition
             simulation.stop();
 
-            labels.filter(d => d.name === "Africa")
-                .text(d => `${d.name}: ${Math.round(d.value)} kWh`);
+            labels.filter(d => d.continent === "Africa")
+                .text(d => `${d.continent}: ${Math.round(d.value)} kWh`);
             // Update the radius of the Africa circle in the SVG with transition
             svg3.select("#africaCircle")
                 .transition()
@@ -253,7 +220,7 @@ function dragended(event, d) {
                 .attr("r", bubbleScale(africa.value))
                 .on("end", () => {
                     // Restart the simulation after the transition completes
-                    simulation.nodes(continentsObj.children).alpha(1).restart();
+                    simulation.nodes(continentObj).alpha(1).restart();
                 });
 
             // Manually updating the collision radius for all nodes in the simulation
@@ -263,22 +230,17 @@ function dragended(event, d) {
             simulation.alpha(1).restart();
         }
 
-
-
         // giving each button in our solar power drop down an eventlistner that listens for when it is clicked. when it is clicked it returns setEnergy
         //which then adds or detracts the right amount of energy from the Africa Bubble 
-        solarBtn0.addEventListener("click", () => {
-            return setEnergy(originalValue)
-        })
-        solarBtn1.addEventListener("click", () => {
-            return setEnergy(521.55 + originalValue)
-        })
-        solarBtn2.addEventListener("click", () => {
-            return setEnergy(1043.1 + originalValue)
-        })
-        solarBtn3.addEventListener("click", () => {
-            return setEnergy(2086.2 + originalValue)
-        })
+        const solarBtns = [solarBtn0, solarBtn1, solarBtn2, solarBtn3];
+        const energyValues = [0, 521.55, 1043.1, 2086.2];
+        
+        solarBtns.forEach((btn, index) => {
+            btn.addEventListener("click", () => {
+                setEnergy(energyValues[index] + originalValue);
+            });
+        });
+        
 
 
     })
@@ -287,11 +249,7 @@ function dragended(event, d) {
     });
 
 //Declaring country insert, which is used to populate continents.Obj
-function CountryInsert(continent, country, totalEnergy, renewable, nuclear, fossil) {
+function ContinentInsert(continent, totalEnergy) {
     this.continent = continent;
-    this.name = country;
     this.value = totalEnergy; // This is important for D3 packing
-    this.renewable = renewable;
-    this.nuclear = nuclear;
-    this.fossil = fossil;
 }
